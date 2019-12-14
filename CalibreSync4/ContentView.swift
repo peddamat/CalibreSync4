08ef0bb31e52cb98fff063c823510f39e8c8396f
Sam Peddamatham <peddamat@gmail.com>
2019-12-13 20:26:30 -0500

    Initial Commit

---
+++ CalibreSync4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalibreSync4
+//
+//  Created by Sumanth Peddamatham on 12/13/19.
+//  Copyright © 2019 Sumanth Peddamatham. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CalibreSync4Tests/CalibreSync4Tests.swift
@@ -0,0 +1,34 @@
+//
+//  CalibreSync4Tests.swift
+//  CalibreSync4Tests
+//
+//  Created by Sumanth Peddamatham on 12/13/19.
+//  Copyright © 2019 Sumanth Peddamatham. All rights reserved.
+//
+
+import XCTest
+@testable import CalibreSync4
+
+class CalibreSync4Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
